29804fde2c56508564d2b3980d2d60d0e534364e
Dmitry Obukhov <stel2k@gmail.com>
2013-05-22 18:56:13 +0400

    Initial import

---
+++ OCRSDKClient.h
@@ -0,0 +1,155 @@
+// OCRSDKClient.h
+//
+// Copyright (c) 2013 Dmitry Obukhov (stel2k@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFHTTPClient.h"
+
+/**
+ `OCRSDKClient` is an `AFHTTPClient` subclass for interacting with the ABBYY Cloud OCR SDK webservice API (http://ocrsdk.com).
+ 
+ See ABBYY Cloud OCR SDK API Reference on http://ocrsdk.com/documentation/apireference.
+ */
+@interface OCRSDKClient : AFHTTPClient
+
+/// @name Accessing HTTP Client Properties
+
+/**
+ Your ABBYY Cloud OCR SDK application ID.
+ */
+@property (nonatomic, readonly) NSString *applicationId;
+
+/**
+ Your ABBYY Cloud OCR SDK application password.
+ */
+@property (nonatomic, readonly) NSString *password;
+
+/**
+ Your ABBYY Cloud OCR SDK application installation ID for current device.
+ 
+ @discussion By default `installationId` is nil, you must call `activateInstallation:success:failure:` to setup its value.
+ */
+@property (nonatomic, readonly) NSString *installationId;
+
+/// @name Creating and Initializing ABBYY Cloud OCR SDK Client
+
+/**
+ Creates and initializes an `OCRSDKClient` object with specified credentials.
+ 
+ @param applicationId Your application ID.
+ @param password Your application password.
+ 
+ @return The newly-initialized ABBYY Cloud OCR SDK client.
+ */
++ (instancetype)clientWithApplicationId:(NSString *)applicationId password:(NSString *)password;
+
+/**
+ Initializes and returns a newly allocated `OCRSDKClient` object with specified credentials.
+ 
+ @param applicationId Your application ID.
+ @param password Your application password.
+ 
+ @return The newly-initialized ABBYY Cloud OCR SDK client.
+ */
+- (id)initWithApplicationId:(NSString *)applicationId password:(NSString *)password;
+
+/// @name Activation Application Installation
+
+/**
+ Activates your application installation on current device.
+ 
+ @param force If YES or installation hasn't already been activated on current device asks server for installation ID, otherwise takes installation ID from user defaults.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and no arguments.
+ @param failure A block object to be executed when the request operation finishes unsuccessfully. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ 
+ @discussion You should call this method before performing any image processing operations.
+ */
+- (void)activateInstallation:(BOOL)force
+					 success:(void (^)(void))success
+					 failure:(void (^)(NSError *error))failure;
+
+/// @name Performing Image Processing Operations
+
+/**
+ Uploads image data to server and starts image processing.
+ 
+ @param imageData An `NSData` object with image data.
+ @param processingParams Image processing params.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the `NSDictionary` object with new image processing task task info.
+
+ @param failure A block object to be executed when the request operation finishes unsuccessfully. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ */
+- (void)startTaskWithImageData:(NSData *)imageData
+					withParams:(NSDictionary *)processingParams
+					   success:(void (^)(NSDictionary *taskInfo))success
+					   failure:(void (^)(NSError *error))failure;
+
+/**
+ Retrives the image processing task info.
+ 
+ @param taskId The ID of image processing task.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the `NSDictionary` object with image processing task task info.
+ 
+ @param failure A block object to be executed when the request operation finishes unsuccessfully. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ */
+- (void)getTaskInfo:(NSString *)taskId
+			success:(void (^)(NSDictionary *taskInfo))success
+			failure:(void (^)(NSError *error))failure;
+
+/**
+ Downloads the recognized data.
+ 
+ @param url URL to download.
+ @param success A block object to be executed when the request operation finishes successfully. This block has no return value and takes a single argument: the `NSData` object with recognized string data.
+ 
+ @param failure A block object to be executed when the request operation finishes unsuccessfully. This block has no return value and takes a single argument: the `NSError` object describing error that occurred.
+ */
+- (void)downloadRecognizedData:(NSURL *)url
+					   success:(void (^)(NSData *downloadedData))success
+					   failure:(void (^)(NSError *error))failure;
+
+@end
+
+/// @name Constants
+
+/**
+ ### Processing task info dictionary keys
+
+ The following constants describes the keys in processing task info dictionary
+ */
+extern NSString * const OCRSDKTaskId;
+extern NSString * const OCRSDKTaskStatus;
+extern NSString * const OCRSDKTaskFilesCount;
+extern NSString * const OCRSDKTaskCredits;
+extern NSString * const OCRSDKTaskRegistrationTime;
+extern NSString * const OCRSDKTaskStatusChangeTime;
+extern NSString * const OCRSDKTaskEstimatedProcessingTime;
+extern NSString * const OCRSDKTaskResultURL;
+
+/**
+ ### Processing task status
+ */
+extern NSString * const OCRSDKTaskStatusSubmitted;
+extern NSString * const OCRSDKTaskStatusQueued;
+extern NSString * const OCRSDKTaskStatusInProgress;
+extern NSString * const OCRSDKTaskStatusCompleted;
+extern NSString * const OCRSDKTaskStatusProcessingFailed;
+extern NSString * const OCRSDKTaskStatusDeleted;
+extern NSString * const OCRSDKTaskStatusNotEnoughCredits;
---
+++ OCRSDKClient.m
@@ -0,0 +1,219 @@
+// OCRSDKClient.m
+//
+// Copyright (c) 2013 Dmitry Obukhov (stel2k@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "OCRSDKClient.h"
+#import "AFHTTPRequestOperation.h"
+#import "XMLDictionary.h"
+
+NSString * const OCRSDKTaskId = @"_id";
+NSString * const OCRSDKTaskStatus = @"_status";
+NSString * const OCRSDKTaskFilesCount = @"_filesCount";
+NSString * const OCRSDKTaskCredits = @"_credits";
+NSString * const OCRSDKTaskRegistrationTime = @"_registrationTime";
+NSString * const OCRSDKTaskStatusChangeTime = @"_statusChangeTime";
+NSString * const OCRSDKTaskEstimatedProcessingTime = @"_estimatedProcessingTime";
+NSString * const OCRSDKTaskResultURL = @"_resultUrl";
+
+NSString * const OCRSDKTaskStatusSubmitted = @"Submitted";
+NSString * const OCRSDKTaskStatusQueued = @"Queued";
+NSString * const OCRSDKTaskStatusInProgress = @"InProgress";
+NSString * const OCRSDKTaskStatusCompleted = @"Completed";
+NSString * const OCRSDKTaskStatusProcessingFailed = @"ProcessingFailed";
+NSString * const OCRSDKTaskStatusDeleted = @"Deleted";
+NSString * const OCRSDKTaskStatusNotEnoughCredits = @"NotEnoughCredits";
+
+static NSString * const kOCRSDKBaseURLString = @"http://cloud.ocrsdk.com";
+static NSString * const kOCRSDKInstallationId = @"com.abbyy.ocrsdk.installation-id";
+
+@implementation OCRSDKClient
+
++ (instancetype)clientWithApplicationId:(NSString *)applicationId password:(NSString *)password
+{
+	return [[OCRSDKClient alloc] initWithApplicationId:applicationId password:password];
+}
+
+- (id)initWithApplicationId:(NSString *)applicationId password:(NSString *)password
+{
+	self = [self initWithBaseURL:[NSURL URLWithString:kOCRSDKBaseURLString]];
+	
+	if (self != nil) {
+		_applicationId = applicationId;
+		_password = password;
+		
+		[self updateAuthorizationHeader];
+	}
+	
+	return self;
+}
+
+- (id)initWithBaseURL:(NSURL *)url
+{
+    self = [super initWithBaseURL:url];
+	
+    if (self != nil) {
+        [self registerHTTPOperationClass:[AFHTTPRequestOperation class]];
+		[self setDefaultHeader:@"Accept" value:@"application/xml"];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+
+- (void)activateInstallation:(BOOL)force
+					 success:(void (^)(void))success
+					 failure:(void (^)(NSError *error))failure
+{
+	
+	NSParameterAssert(self.applicationId);
+	NSParameterAssert(self.password);
+	
+	if (self.installationId == nil || force) {
+		NSString *deviceId = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+		
+		[self getPath:@"activateNewInstallation" parameters:@{@"deviceId": deviceId} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+			NSDictionary *responseDictionary = [NSDictionary dictionaryWithXMLData:responseObject];
+			
+			NSString *installationId = [responseDictionary valueForKey:@"authToken"];
+			[[NSUserDefaults standardUserDefaults] setObject:installationId forKey:kOCRSDKInstallationId];
+			
+			NSParameterAssert(self.installationId);
+			
+			[self activateInstallation:NO success:success failure:failure];
+		} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+			if (failure != nil) {
+				failure(error);
+			}
+		}];
+	} else {
+		[self updateAuthorizationHeader];
+		
+		if (success != nil) {
+			success();
+		}
+	}
+}
+
+- (void)startTaskWithImageData:(NSData *)imageData
+					withParams:(NSDictionary *)processingParams
+					   success:(void (^)(NSDictionary *taskInfo))success
+					   failure:(void (^)(NSError *error))failure
+{
+	NSParameterAssert(imageData);
+	
+	// Create a GET request to make AFHTTPClient set params to url
+	NSMutableURLRequest *request = [self requestWithMethod:@"GET" path:@"processImage" parameters:processingParams];
+	[request setHTTPMethod:@"POST"];
+	[request setValue:@"applicaton/octet-stream" forHTTPHeaderField:@"Content-Type"];
+	[request setHTTPBody:imageData];
+	
+	AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+	
+	[operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+		if (success != nil) {
+			NSDictionary *responseDictionary = [NSDictionary dictionaryWithXMLData:responseObject];
+			NSDictionary *taskInfo = [responseDictionary objectForKey:@"task"];
+			
+			success(taskInfo);
+		}
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		if (failure != nil) {
+			failure(error);
+		}
+	}];
+	
+	[self enqueueHTTPRequestOperation:operation];
+}
+
+- (void)getTaskInfo:(NSString *)taskId
+			success:(void (^)(NSDictionary *taskInfo))success
+			failure:(void (^)(NSError *error))failure
+{
+	NSParameterAssert(taskId);
+	
+	[self getPath:@"getTaskStatus" parameters:@{@"taskId": taskId} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+		if (success != nil) {
+			NSDictionary *responseDictionary = [NSDictionary dictionaryWithXMLData:responseObject];
+			NSDictionary *taskInfo = [responseDictionary objectForKey:@"task"];
+			
+			success(taskInfo);
+		}
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		if (failure != nil) {
+			failure(error);
+		}
+	}];
+}
+
+- (void)downloadRecognizedData:(NSURL *)url
+					   success:(void (^)(NSData *downloadedData))success
+					   failure:(void (^)(NSError *error))failure
+{
+	NSParameterAssert(url);
+	
+	NSURLRequest *request = [NSURLRequest requestWithURL:url];
+	
+	AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+	
+	[operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+		if (success != nil) {
+			success(responseObject);
+		}
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		if (failure != nil) {
+			failure(error);
+		}
+	}];
+	
+	[self enqueueHTTPRequestOperation:operation];
+}
+
+#pragma mark -
+
+- (void)setApplicationId:(NSString *)applicationId
+{
+	if (applicationId != self.applicationId) {
+		_applicationId = applicationId;
+		[self updateAuthorizationHeader];
+	}
+}
+
+- (void)setPassword:(NSString *)password
+{
+	if (password != self.password) {
+		_password = password;
+		[self updateAuthorizationHeader];
+	}
+}
+
+- (NSString *)installationId
+{
+	return [[NSUserDefaults standardUserDefaults] stringForKey:kOCRSDKInstallationId];
+}
+
+- (void)updateAuthorizationHeader
+{
+	[self setAuthorizationHeaderWithUsername:self.installationId != nil ? [self.applicationId stringByAppendingString:self.installationId] : self.applicationId
+									password:self.password];
+}
+
+@end
