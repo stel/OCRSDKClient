6377ea4bfa53279d5ba76d27e58ac974dc866ee5
Dmitry Obukhov <stel2k@gmail.com>
2013-05-22 22:01:57 +0400

    Xcode project fixes

--- OCRSDKClient/OCRSDKClient.h
+++ OCRSDKClient/OCRSDKClient.h
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 #import "AFHTTPClient.h"
 
 /**
